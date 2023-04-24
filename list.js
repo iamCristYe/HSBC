a750736a32745672e818352f38a2a6ada4d09850
Crist Ye <iamCristYe@gmail.com>
2023-04-24 09:40:16 +0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,400 @@
+onThemeReady(function (view) {
+    bankingRoute = new BankingRoute([{
+        "id": "bc912c6e-11fb-49c1-988f-2d7bb5f2ad1f",
+        "title": "Personal banking",
+        "countries": [{
+            "id": "AR",
+            "title": "Argentina",
+            "action": "https://www.hsbc.com.ar/",
+            "filterBy": ""
+        },
+        {
+            "id": "AM",
+            "title": "Armenia",
+            "action": "https://www.hsbc.am/en-am/",
+            "filterBy": ""
+        },
+        {
+            "id": "AU",
+            "title": "Australia",
+            "action": "https://www.hsbc.com.au/",
+            "filterBy": ""
+        },
+        {
+            "id": "BH",
+            "title": "Bahrain",
+            "action": "https://www.hsbc.com.bh/",
+            "filterBy": ""
+        },
+        {
+            "id": "BD",
+            "title": "Bangladesh",
+            "action": "https://www.hsbc.com.bd/1/2/",
+            "filterBy": ""
+        },
+        {
+            "id": "BM",
+            "title": "Bermuda",
+            "action": "https://www.hsbc.bm/",
+            "filterBy": ""
+        },
+        {
+            "id": "CA",
+            "title": "Canada",
+            "action": "https://www.hsbc.ca/",
+            "filterBy": ""
+        },
+        {
+            "id": "EG",
+            "title": "Egypt",
+            "action": "https://www.hsbc.com.eg/1/2/eg/personal",
+            "filterBy": ""
+        },
+        {
+            "id": "FR",
+            "title": "France",
+            "action": "https://www.hsbc.fr/en-fr/",
+            "filterBy": ""
+        },
+        {
+            "id": "GR",
+            "title": "Greece",
+            "action": "https://www.hsbc.gr/en-gr/",
+            "filterBy": ""
+        },
+        {
+            "id": "GG",
+            "title": "Guernsey",
+            "action": "https://ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "HK",
+            "title": "Hong Kong",
+            "action": "https://www.hsbc.com.hk/",
+            "filterBy": ""
+        },
+        {
+            "id": "HSEX",
+            "title": "HSBC Expat",
+            "action": "https://www.expat.hsbc.com/1/2/",
+            "filterBy": ""
+        },
+        {
+            "id": "IN",
+            "title": "India",
+            "action": "https://www.hsbc.co.in/",
+            "filterBy": ""
+        },
+        {
+            "id": "ID",
+            "title": "Indonesia",
+            "action": "https://www.hsbc.co.id/1/2/home_en_US",
+            "filterBy": ""
+        },
+        {
+            "id": "IM",
+            "title": "Isle of Man",
+            "action": " https://ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "JE",
+            "title": "Jersey",
+            "action": "https://ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "MO",
+            "title": "Macau",
+            "action": "https://www.hsbc.com.mo/",
+            "filterBy": ""
+        },
+        {
+            "id": "CN",
+            "title": "Mainland China",
+            "action": "https://www.hsbc.com.cn/en-cn/",
+            "filterBy": ""
+        },
+        {
+            "id": "MY",
+            "title": "Malaysia",
+            "action": "https://www.hsbc.com.my/ ",
+            "filterBy": ""
+        },
+        {
+            "id": "MT",
+            "title": "Malta",
+            "action": "https://www.hsbc.com.mt/",
+            "filterBy": ""
+        },
+        {
+            "id": "MU",
+            "title": "Mauritius",
+            "action": "https://www.hsbc.co.mu/",
+            "filterBy": ""
+        },
+        {
+            "id": "MX",
+            "title": "Mexico",
+            "action": "https://www.hsbc.com.mx/",
+            "filterBy": ""
+        },
+        {
+            "id": "NZ",
+            "title": "New Zealand",
+            "action": " https://www.hsbc.co.nz/",
+            "filterBy": ""
+        },
+        {
+            "id": "OM",
+            "title": "Oman",
+            "action": "https://www.oman.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "PH",
+            "title": "Philippines",
+            "action": "https://www.hsbc.com.ph/ ",
+            "filterBy": ""
+        },
+        {
+            "id": "QA",
+            "title": "Qatar",
+            "action": "https://www.hsbc.com.qa/",
+            "filterBy": ""
+        },
+        {
+            "id": "SA",
+            "title": "Saudi Arabia",
+            "action": "https://www.sab.com/en/",
+            "filterBy": ""
+        },
+        {
+            "id": "SG",
+            "title": "Singapore",
+            "action": " https://www.hsbc.com.sg/",
+            "filterBy": ""
+        },
+        {
+            "id": "LK",
+            "title": "Sri Lanka",
+            "action": "https://www.hsbc.lk/",
+            "filterBy": ""
+        },
+        {
+            "id": "TW",
+            "title": "Taiwan",
+            "action": "https://www.hsbc.com.tw/en-tw/",
+            "filterBy": ""
+        },
+        {
+            "id": "TR",
+            "title": "Türkiye",
+            "action": "https://www.hsbc.com.tr/en/ ",
+            "filterBy": "turkey"
+        },
+        {
+            "id": "AE",
+            "title": "UAE",
+            "action": "https://www.hsbc.ae/ ",
+            "filterBy": ""
+        },
+        {
+            "id": "GB",
+            "title": "UK",
+            "action": "https://www.hsbc.co.uk/",
+            "filterBy": ""
+        },
+        {
+            "id": "UKF",
+            "title": "UK - first direct",
+            "action": "https://www1.firstdirect.com/ ",
+            "filterBy": ""
+        },
+        {
+            "id": "UY",
+            "title": "Uruguay",
+            "action": "https://www.hsbc.com.uy/ ",
+            "filterBy": ""
+        },
+        {
+            "id": "US",
+            "title": "USA",
+            "action": "https://www.us.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "VN",
+            "title": "Vietnam",
+            "action": " https://www.hsbc.com.vn/en-vn/",
+            "filterBy": ""
+        }],
+        "popular": ["HK",
+            "GB",
+            "US"],
+        "action": null
+    },
+    {
+        "id": "a356df90-c858-4d39-84f9-90b63af48231",
+        "title": "Business banking",
+        "countries": [{
+            "id": "AR",
+            "title": "Argentina",
+            "action": "https://www.hsbc.com.ar/empresas/index.html",
+            "filterBy": ""
+        },
+        {
+            "id": "AU",
+            "title": "Australia",
+            "action": "https://www.business.hsbc.com.au/en-au",
+            "filterBy": ""
+        },
+        {
+            "id": "CA",
+            "title": "Canada",
+            "action": "https://www.business.hsbc.ca/en-ca",
+            "filterBy": ""
+        },
+        {
+            "id": "FR",
+            "title": "France",
+            "action": "http://www.business.hsbc.fr/fr-fr",
+            "filterBy": ""
+        },
+        {
+            "id": "DE",
+            "title": "Germany",
+            "action": "https://www.firmenkunden.hsbc.de/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "GG",
+            "title": "Guernsey",
+            "action": "https://www.business.ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "HK",
+            "title": "Hong Kong",
+            "action": "https://www.business.hsbc.com.hk/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "HKHS",
+            "title": "Hong Kong - Hang Seng",
+            "action": "https://www.business.hsbc.com.hk/",
+            "filterBy": ""
+        },
+        {
+            "id": "IN",
+            "title": "India",
+            "action": "https://www.business.hsbc.co.in/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "IM",
+            "title": "Isle of Man",
+            "action": "https://www.business.ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "JE",
+            "title": "Jersey",
+            "action": "https://www.business.ciiom.hsbc.com/",
+            "filterBy": ""
+        },
+        {
+            "id": "CN",
+            "title": "Mainland China",
+            "action": "https://www.business.hsbc.com.cn/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "MY",
+            "title": "Malaysia",
+            "action": "https://www.business.hsbc.com.my/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "MT",
+            "title": "Malta",
+            "action": "https://www.business.hsbc.com.mt/",
+            "filterBy": ""
+        },
+        {
+            "id": "OM",
+            "title": "Oman",
+            "action": "https://www.business.hsbc.co.om/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "PH",
+            "title": "Philippines",
+            "action": "https://www.hsbc.com.ph/",
+            "filterBy": ""
+        },
+        {
+            "id": "QA",
+            "title": "Qatar",
+            "action": "https://www.business.hsbc.com.qa/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "SG",
+            "title": "Singapore",
+            "action": "https://www.business.hsbc.com.sg/",
+            "filterBy": ""
+        },
+        {
+            "id": "TR",
+            "title": "Türkiye",
+            "action": "https://www.business.hsbc.com.tr/en-gb",
+            "filterBy": "turkey"
+        },
+        {
+            "id": "AE",
+            "title": "UAE",
+            "action": "https://www.business.hsbc.ae/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "GB",
+            "title": "UK",
+            "action": "https://www.business.hsbc.uk/en-gb",
+            "filterBy": ""
+        },
+        {
+            "id": "US",
+            "title": "USA",
+            "action": "https://www.business.us.hsbc.com/en",
+            "filterBy": ""
+        },
+        {
+            "id": "VN",
+            "title": "Vietnam",
+            "action": "https://www.business.hsbc.com.vn/en-gb",
+            "filterBy": ""
+        }],
+        "popular": ["HK",
+            "GB",
+            "US"],
+        "action": null
+    },
+    {
+        "id": "d4c16a9c-01df-4f63-80b7-bcf4d8e705c1",
+        "title": "Corporate and institutional banking",
+        "countries": [],
+        "popular": [],
+        "action": "http://www.hsbcnet.com/"
+    },
+    {
+        "id": "6478c197-ff9a-48ff-8d22-bb4bc7241edc",
+        "title": "Private banking",
+        "countries": [],
+        "popular": [],
+        "action": "https://www.privatebanking.hsbc.com/"
+    }],
+        "bc912c6e-11fb-49c1-988f-2d7bb5f2ad1f");
+    bankingRoute.activate(view);
+});
